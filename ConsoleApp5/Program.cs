319631c8f2b0349196e0105c9760ee8a8128b9dc
Artem4587 <artemneznayou@gmail.com>
2025-06-04 10:39:38 +0700

    gwrghg

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+enum EducationForm { Очная, Заочная, Вечерняя }
+
+class Student
+{
+    public string Name { get; set; }
+    public int Course { get; set; }
+    public EducationForm Form { get; set; }
+
+    public void Study()
+    {
+        Console.WriteLine($"{Name} учится на {Course} курсе, форма обучения: {Form}");
+    }
+}
+class Example
+{
+    public static void Main(string[] args)
+    {
+        Student student = new Student { Name = "Иван", Course = 3, Form = EducationForm.Очная };
+        student.Study();
+    }
+}
