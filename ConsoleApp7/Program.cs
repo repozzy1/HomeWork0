fd5406332b9c1cbde5671089d3565bc0d6291d8c
Artem4587 <artemneznayou@gmail.com>
2025-06-04 10:45:42 +0700

    gegehge

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double GPA { get; set; } //Успеваемость
+}
+
+static class StudentExtensions
+{
+    public static void PrintInfo(this Student student)
+    {
+        Console.WriteLine($"Полное имя: {student.FirstName} {student.LastName}, Успеваемость: {student.GPA}");
+    }
+}
+
+class Example
+{
+    public static void Main(string[] args)
+    {
+        Student student = new Student { FirstName = "Иван", LastName = "Иванов", GPA = 4.5 };
+        student.PrintInfo(); // Вызов extension-метода
+    }
+}
